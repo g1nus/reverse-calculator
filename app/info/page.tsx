6ab1966261cfb263e28d8f621ba67ab486b9af6c
g1nus <giuliano.andronic@studenti.unitn.it>
2023-05-04 12:35:59 +0200

    add basic info page

---
+++ app/info/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{width: `100vw`}}>
+        Author - Giuliano Andronic <br></br>
+        Github Repo - <a href="https://github.com/g1nus/reverse-calculator">https://github.com/g1nus/reverse-calculator</a>
+    </main>
+  )
+}
