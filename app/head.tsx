1cab709f16233c8a2a9e0d8d53dcc8b3a6eae292
g1nus <giuliano.andronic@studenti.unitn.it>
2023-06-30 16:00:44 +0300

    add basic dark mode

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,7 @@ export default function Head() {
   return (
     <>
       <title>Anti Calculator</title>
+      <meta name="color-scheme" content="dark light"></meta>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="A reverse calculator game" />
       <link rel="icon" href="/favicon.ico" />
